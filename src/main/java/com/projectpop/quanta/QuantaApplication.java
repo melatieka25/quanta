a15bbb2084d6069f17109b1be512f74ec347e88c
Yelda Faizah Havara <yelda.faizah@ui.ac.id>
2023-02-23 16:09:22 +0700

    initial commit springboot

---
+++ src/main/java/com/projectpop/quanta/QuantaApplication.java
@@ -0,0 +1,13 @@
+package com.projectpop.quanta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectpop/quanta/QuantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectpop.quanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
