25aef52442f6c9d6f31dec424325b42e8ee27ef5
Melati Eka Putri <melatieka25@gmail.com>
2023-03-09 14:33:42 +0700

    Add logout functionality

--- src/main/java/com/projectpop/quanta/user/controller/PageController.java
+++ src/main/java/com/projectpop/quanta/user/controller/PageController.java
@@ -1,16 +1,28 @@
 package com.projectpop.quanta.user.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.projectpop.quanta.user.model.UserModel;
+import com.projectpop.quanta.user.service.UserService;
 
 @Controller
 public class PageController {
 
     @Autowired
     ServerProperties serverProperties;
+
+    @Qualifier("userServiceImpl")
+    @Autowired
+    private UserService userService;
     
     @RequestMapping("/")
     public String home() {
@@ -22,5 +34,14 @@ public class PageController {
         model.addAttribute("port", serverProperties.getPort());
         return "auth/login";
     }
+
+    @GetMapping (value = "/logout-sso")
+    public ModelAndView logoutSSO(Principal principal) {
+        UserModel user = userService.getUserByEmail(principal.getName());
+        System.out.println(user.getEmail());
+        
+        return new ModelAndView("redirect:/logout");
+    }
+
 
 }
