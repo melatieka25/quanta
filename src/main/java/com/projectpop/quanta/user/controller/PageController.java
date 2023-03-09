9ae8b792594d3de825d45d3942ec8a6f3d0a3717
Melati Eka Putri <melati.eka@ui.ac.id>
2023-03-09 21:12:39 +0700

    Feat: Add Login, Logout

--- src/main/java/com/projectpop/quanta/user/controller/PageController.java
+++ src/main/java/com/projectpop/quanta/user/controller/PageController.java
@@ -1,13 +1,47 @@
 package com.projectpop.quanta.user.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.projectpop.quanta.user.model.UserModel;
+import com.projectpop.quanta.user.service.UserService;
 
 @Controller
 public class PageController {
+
+    @Autowired
+    ServerProperties serverProperties;
+
+    @Qualifier("userServiceImpl")
+    @Autowired
+    private UserService userService;
+    
     @RequestMapping("/")
     public String home() {
         return "home";
     }
+
+    @RequestMapping("/login")
+    public String login(Model model){
+        model.addAttribute("port", serverProperties.getPort());
+        return "auth/login";
+    }
+
+    @GetMapping (value = "/logout")
+    public ModelAndView logout(Principal principal) {
+        UserModel user = userService.getUserByEmail(principal.getName());
+        System.out.println(user.getEmail());
+        
+        return new ModelAndView("redirect:/logout");
+    }
+
 
 }
