c2047cc716a2f8cac939971ff972f6b181ebf865
Yelda Faizah Havara <yelda.faizah@ui.ac.id>
2023-03-19 21:02:07 +0700

    Feat/epic5 manajemen mata pelajaran

---
+++ src/main/java/com/projectpop/quanta/pengajarmapel/service/PengajarMapelService.java
@@ -0,0 +1,7 @@
+package com.projectpop.quanta.pengajarmapel.service;
+
+import com.projectpop.quanta.mapel.model.MataPelajaranModel;
+
+public interface PengajarMapelService {
+    void deleteAllByMapel(MataPelajaranModel mapel);
+}
