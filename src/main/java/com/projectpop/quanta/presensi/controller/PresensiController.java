a4bf7be40b6a8770b115ac6fc015902163b74a7f
PRIYANKA DEVI <priyanka.devi@ui.ac.id>
2023-03-31 14:06:45 +0700

    Hotfix: Add email for every students in data table, fix text alignment, and...

--- src/main/java/com/projectpop/quanta/presensi/controller/PresensiController.java
+++ src/main/java/com/projectpop/quanta/presensi/controller/PresensiController.java
@@ -10,8 +10,11 @@ import com.projectpop.quanta.presensi.service.PresensiServiceImpl;
 import com.projectpop.quanta.siswa.model.SiswaModel;
 import com.projectpop.quanta.siswa.service.SiswaServiceImpl;
 import com.projectpop.quanta.siswajadwalkelas.model.SiswaJadwalModel;
+import com.projectpop.quanta.tahunajar.model.TahunAjarModel;
+import com.projectpop.quanta.tahunajar.service.TahunAjarServiceImpl;
 import com.projectpop.quanta.user.model.UserModel;
 import com.projectpop.quanta.user.service.UserServiceImpl;
+import io.netty.util.internal.StringUtil;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -21,6 +24,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.thymeleaf.util.StringUtils;
 
 import java.security.Principal;
 import java.text.ParseException;
@@ -29,6 +34,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
 import java.util.*;
 
 @Controller
@@ -46,9 +52,8 @@ public class PresensiController {
     @Autowired
     private UserServiceImpl userService;
 
-    @Qualifier("pengajarServiceImpl")
-    @Autowired
-    private PengajarServiceImpl pengajarService;
+    @Qualifier("tahunAjarServiceImpl")
+    private TahunAjarServiceImpl tahunAjarService;
 
     @GetMapping("")
     public String viewAllJadwalPengajarHariIni(Model model, Principal principal) throws ParseException {
@@ -111,7 +116,11 @@ public class PresensiController {
             }
         }
         model.addAttribute("tahunAjaran", tahunAjaran);
-        model.addAttribute("tanggal", LocalDate.now());
+        model.addAttribute("hari", StringUtils.capitalize(LocalDate.now().getDayOfWeek().name().toLowerCase()));
+        model.addAttribute("tanggal", LocalDate.now().getDayOfMonth());
+        model.addAttribute("bulan", StringUtils.capitalize(LocalDate.now().getMonth().name().toLowerCase()));
+        model.addAttribute("tahun", LocalDate.now().getYear());
+//        model.addAttribute("tanggal", localDateTimeToDateWithDash(LocalDateTime.now()));
         model.addAttribute("jadwalKelasHariIniMap", jadwalKelasHariIniMap);
         model.addAttribute("allJadwalKelasMap", allJadwalKelasMap);
         return "presensi/landing-page";
@@ -124,37 +133,33 @@ public class PresensiController {
     }
     @GetMapping(value = "/read/{idJadwalKelas}")
     public String viewDetailPresensi(@PathVariable("idJadwalKelas") Integer idJadwalKelas, Model model){
-        String tahunAjaran;
-        int currentYear = LocalDate.now().getYear();
-        int yearBefore = LocalDate.now().minusYears(1).getYear();
-        int yearAfter = LocalDate.now().plusYears(1).getYear();
-        if (9<=LocalDate.now().getMonthValue() || LocalDate.now().getMonthValue()==1){
-            tahunAjaran = currentYear + "/" + yearAfter;
-        }
-        else{
-            tahunAjaran = yearBefore + "/" +currentYear;
-        }
         JadwalKelasModel jadwalKelasModel = jadwalKelasService.getJadwalKelasById(idJadwalKelas);
+        TahunAjarModel tahunAjaran = jadwalKelasModel.getKelas().getTahunAjar();
         model.addAttribute("jadwalKelasModel", jadwalKelasModel);
         model.addAttribute("idJadwalKelas", idJadwalKelas);
-        model.addAttribute("tahunAjaran", tahunAjaran);
-        model.addAttribute("tanggal", LocalDate.now());
+        model.addAttribute("hari", StringUtils.capitalize(jadwalKelasModel.getStartDateClass().getDayOfWeek().name().toLowerCase()));
+        model.addAttribute("tanggal", jadwalKelasModel.getStartDateClass().getDayOfMonth());
+        model.addAttribute("bulan", StringUtils.capitalize(jadwalKelasModel.getStartDateClass().getMonth().name().toLowerCase()));
+        model.addAttribute("tahun", jadwalKelasModel.getStartDateClass().getYear());
+        model.addAttribute("tahunAjaran", tahunAjaran.getName());
         model.addAttribute("kelas", jadwalKelasModel.getKelas());
         model.addAttribute("presensiModelList", jadwalKelasModel.getListPresensi());
         System.out.println(jadwalKelasModel.getListPresensi());
         return "presensi/read-presensi-kelas";
     }
     @GetMapping("/{idJadwalKelas}")
-    public String updatePresensiKelasFormPage(@PathVariable String idJadwalKelas, Model model){
+    public String updatePresensiKelasFormPage(@PathVariable String idJadwalKelas, Model model, RedirectAttributes redirectAttributes){
         String tahunAjaran;
         JadwalKelasModel jadwalKelasModel = jadwalKelasService.getJadwalKelasById(Integer.parseInt(idJadwalKelas));
         List<PresensiModel> presensiModelList = jadwalKelasModel.getListPresensi();
         var userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         var userModel = userService.getUserByEmail(userDetails.getUsername());
         if (!userModel.getEmail().equals(jadwalKelasModel.getPengajarKelas().getEmail())){
+            redirectAttributes.addFlashAttribute("error", "Anda tidak dapat mengubah presensi kelas yang tidak anda ajarkan");
             return "redirect:/presensi/read/"+jadwalKelasModel.getId();
         }
         if(!localDateTimeToDateWithSlash(jadwalKelasModel.getStartDateClass()).equals(localDateTimeToDateWithSlash(LocalDateTime.now()))){
+            redirectAttributes.addFlashAttribute("error", "Anda hanya dapat mengubah presensi kelas yang anda ajarkan hari ini");
             return "redirect:/presensi/read/"+jadwalKelasModel.getId();
         }
         int currentYear = LocalDate.now().getYear();
@@ -167,8 +172,12 @@ public class PresensiController {
             tahunAjaran = yearBefore + "/" +currentYear;
         }
         model.addAttribute("listPresensiStatus", PresensiStatus.values());
-        model.addAttribute("tahunAjaran", tahunAjaran);
-        model.addAttribute("tanggal", LocalDate.now());
+        model.addAttribute("hari", StringUtils.capitalize(jadwalKelasModel.getStartDateClass().getDayOfWeek().name().toLowerCase()));
+        model.addAttribute("tanggal", jadwalKelasModel.getStartDateClass().getDayOfMonth());
+        model.addAttribute("bulan", StringUtils.capitalize(jadwalKelasModel.getStartDateClass().getMonth().name().toLowerCase()));
+        model.addAttribute("tahun", jadwalKelasModel.getStartDateClass().getYear());
+        model.addAttribute("tahunAjaran", jadwalKelasModel.getKelas().getTahunAjar().getName());
+//        model.addAttribute("tahunAjaran", tahunAjaran);
         model.addAttribute("kelas", jadwalKelasModel.getKelas());
         model.addAttribute("presensiModelList", presensiModelList);
         model.addAttribute("presensiFilled", false);
@@ -176,7 +185,7 @@ public class PresensiController {
         return "presensi/presensi-kelas";
     }
     @PostMapping(value = "/read/{idJadwalKelas}", params = {"save"})
-    public String updatePresensiKelasSubmitPage(@PathVariable String idJadwalKelas, Model model,  @ModelAttribute JadwalKelasModel jadwalKelasModel){
+    public String updatePresensiKelasSubmitPage(@PathVariable String idJadwalKelas, Model model,  @ModelAttribute JadwalKelasModel jadwalKelasModel, RedirectAttributes redirectAttrs){
         String tahunAjaran;
         int currentYear = LocalDate.now().getYear();
         JadwalKelasModel jadwalKelasModelUpdated = jadwalKelasService.getJadwalKelasById(Integer.parseInt(idJadwalKelas));
@@ -202,6 +211,7 @@ public class PresensiController {
         model.addAttribute("tanggal", LocalDate.now());
         model.addAttribute("kelas", jadwalKelasModelUpdated.getKelas());
         model.addAttribute("presensiModelList", jadwalKelasModelUpdated.getListPresensi());
+        redirectAttrs.addFlashAttribute("success", "Presensi berhasil disimpan");
         return "redirect:/presensi/read/"+jadwalKelasModelUpdated.getId();
     }
 
