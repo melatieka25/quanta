970a3b0cb637c61cff49ea3942904f52b5aa31b0
Melati Eka Putri <melatieka25@gmail.com>
2023-04-26 01:28:06 +0700

    hotfix: delete backend input check

--- src/main/java/com/projectpop/quanta/user/auth/PasswordManager.java
+++ src/main/java/com/projectpop/quanta/user/auth/PasswordManager.java
@@ -7,30 +7,6 @@ import org.apache.commons.text.RandomStringGenerator;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordManager {
-    //Referensi: https://codescracker.com/java/program/java-check-password-strength.htm
-    public static boolean validationChecker(String password){
-        int passwordLength = password.length();
-        int upChars = 0, lowChars = 0, digits = 0, special = 0;
-        if(passwordLength < 8) {
-            return false;
-        } else {
-            for(int i=0; i<passwordLength; i++) {
-                Character ch = password.charAt(i);
-                if(Character.isUpperCase(ch))
-                    upChars = 1;
-                else if(Character.isLowerCase(ch))
-                    lowChars = 1;
-                else if(Character.isDigit(ch))
-                    digits = 1;
-                else
-                    special = 1;
-            }
-        }
-        if(upChars==1 && lowChars==1 && digits==1 && special==1)
-            return true;
-        else
-            return false;
-    }
 
     public static String encrypt(String password){
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
